c4810ca3383385a9ef8755784f61b7184d14960c
owenbyram-spec <owenbyram@gmail.com>
2026-02-13 14:18:10 -0600

    Update db.py

--- app/db.py
+++ app/db.py
@@ -8,4 +8,11 @@ if not settings.database_url:
 
 engine = create_engine(settings.database_url, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
